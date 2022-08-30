417c01c73797030622ee3d15dd7879d65d51e977
wonjongseo <visionwill@naver.com>
2022-08-31 00:52:56 +0900

    TypeScript and React native
    Bottom nav and JSDOC

---
+++ typescript/typeChain/recup/myPackage.js
@@ -0,0 +1,30 @@
+// @ts-check
+
+// 자바스크립트 파일에서 @ts-check 주석을 붙이면
+// 해당 자바스크립트 파일도 타입을 검사함.
+
+// 그전 tsconfig.json 에 "allowJs" : true
+
+// JSDoc 읽어보길
+
+// 요딴 식으로도 설명 가능
+
+/**
+ * Initialize Project
+ * @param {Object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns  { boolean}
+ */
+export function init(config) {
+  return true;
+}
+
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns {number}
+ */
+export function exit(code) {
+  return code + 1;
+}
